43491dd4f337884005975ef8c14169feb52af75e
Erhan Tezcan <erhany96@gmail.com>
2022-04-02 16:58:46 +0300

    refaktor

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,5 @@
-import Alert from './alert'
-import Footer from './footer'
-import Meta from './meta'
+import Footer from "./footer"
+import Meta from "./meta"
 
 type Props = {
   preview?: boolean
@@ -12,7 +11,6 @@ const Layout = ({ preview, children }: Props) => {
     <>
       <Meta />
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,17 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../types/post'
+import Container from "../components/container"
+import MoreStories from "../components/more-stories"
+import Intro from "../components/intro"
+import Layout from "../components/layout"
+import { getAllPosts } from "../lib/api"
+import Head from "next/head"
+import { CMS_NAME } from "../lib/constants"
+import Post from "../types/post"
 
 type Props = {
   allPosts: Post[]
 }
 
 const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout>
@@ -23,17 +20,7 @@ const Index = ({ allPosts }: Props) => {
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {allPosts.length > 0 && <MoreStories posts={allPosts} />}
         </Container>
       </Layout>
     </>
@@ -43,14 +30,7 @@ const Index = ({ allPosts }: Props) => {
 export default Index
 
 export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
+  const allPosts = getAllPosts(["title", "date", "slug", "author", "coverImage", "excerpt"])
 
   return {
     props: { allPosts },
