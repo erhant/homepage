eb703f2011cf48c9df391bddc8ff94420c8365fc
Erhan Tezcan <erhany96@gmail.com>
2023-02-17 15:37:22 +0300

    fix

--- pages/tools/share-local-storage.tsx
+++ pages/tools/share-local-storage.tsx
@@ -1,52 +1,58 @@
 import { Title, Text } from "@mantine/core"
 import Head from "next/head"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Layout from "../../components/layout"
 
-const domains = ["localhost:3000"]
-
-function messageHandler(
-  event: MessageEvent<{
-    action: "get" | "save" | "returnData"
-    key: string
-    value: any
-  }>
-) {
-  // must be an allowed domain
-  if (!domains.includes(event.origin)) {
-    throw Error("Domain not allowed")
-  }
-
-  // get event
-  const { action, key, value } = event.data
-  console.log("Got event data:", event.data)
-  if (action == "save") {
-    // sender wants to save a value in this local storage
-    window.localStorage.setItem(key, JSON.stringify(value))
-  } else if (action == "get" && event.source) {
-    // sender wants to load a value from this local storage
-    let value = window.localStorage.getItem(key)
-
-    // we convert the value to JSON if it has the correct from
-    if (value && typeof value !== "string") {
-      value = JSON.parse(value)
-    } else {
-      value = null
-    }
-
-    // postMessage to the sender
-    event.source.postMessage(
-      {
-        action: "returnData",
-        key,
-        value,
-      },
-      "*" as any // TODO: use actual domain here
-    )
-  }
-}
+const DOMAINS = ["http://localhost:3000"]
 
 const ShareLocalStorage = () => {
+  function messageHandler(
+    event: MessageEvent<{
+      action: "get" | "save" | "returnData"
+      key: string
+      value: any
+    }>
+  ) {
+    // must be an allowed domain
+    console.log("Got event data:", event.data)
+    console.log("Sender:", event.origin)
+    if (!DOMAINS.includes(event.origin)) {
+      throw Error("Domain not allowed")
+    }
+
+    // get event
+    const { action, key, value } = event.data
+
+    if (action == "save") {
+      // sender wants to save a value in this local storage
+      window.localStorage.setItem(key, JSON.stringify(value))
+    } else if (action == "get" && event.source) {
+      // sender wants to load a value from this local storage
+      let value = window.localStorage.getItem(key)
+
+      // we convert the value to JSON if it has the correct from
+      if (value && typeof value !== "string") {
+        value = JSON.parse(value)
+      } else {
+        value = null
+      }
+
+      // postMessage to the sender
+      event.source.postMessage(
+        {
+          action: "returnData",
+          key,
+          value,
+        },
+        "*" as any // TODO: use actual domain here
+      )
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("message", messageHandler, false)
+  }, [])
+
   return (
     <Layout>
       <Head>
